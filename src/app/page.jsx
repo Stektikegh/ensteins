6dac944f240e5613ee9163bacd72c6620b0fedf1
Ghiles <gtouzi035@gmail.com>
2025-12-03 22:30:50 +0100

    added scroll bar colors merged the carousel

---
+++ src/app/page.jsx
@@ -0,0 +1,28 @@
+import Logo from "@/src/components/Logo"
+import NavigationBar from "@/src/components/NavigationBar";
+import BackgroundPaths from "@/src/components/BackgroundPaths";
+import Carousel from "@/src/components/Carousel";
+
+
+
+
+export default function Home() {
+  return (
+    <main className="h-[100vh] w-[100vw] bg-black z-0">
+
+      <BackgroundPaths />
+      <div className="fixed w-full bg-black z-100">
+        <NavigationBar />
+      </div>
+      <div className="flex justify-center items-center w-full h-full">
+        <div className="w-[20vw] relative">
+          <Logo />
+        </div>
+      </div>
+
+      <div className="bg-black w-full h-full flex justify-center p-[100px]">
+        <Carousel slides={["https://picsum.dev/200/100", "https://picsum.dev/200/99", "https://picsum.dev/200/98"]} />
+      </div>
+    </main >
+  );
+}
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
+export default function Carousel({ slides }) {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    appenDots: (dots) => (
+      <div
+        style={{
+          backgroundColor: "white"
+        }}
+      >
+        <ul style={{ margin: "0px" }}> {dots} </ul>
+      </div>
+    )
+  };
+
+  return (
+    <div className="flex flex-col w-[80%] pb-20">
+      <h3 className="w-full text-center mb-15 font-bold text-5xl text-SpecialBlue">MEMORIES</h3>
+      <Slider {...settings}>
+        {slides.map((url, index) => (
+          <div key={index} className="rounded-3xl overflow-hidden">
+            <img
+              src={url}
+              alt={`Slide ${index + 1}`}
+              style={{ width: "100%", height: "70%", objectFit: "cover" }}
+            />
+          </div>
+        ))}
+      </Slider>
+      <button className="mt-17 bg-SpecialBlue text-white font-semibold py-3 px-6 rounded-full self-center hover:bg-[#256fbb] transition-colors duration-300">
+        More Events
+      </button>
+    </div>
+  );
+}
